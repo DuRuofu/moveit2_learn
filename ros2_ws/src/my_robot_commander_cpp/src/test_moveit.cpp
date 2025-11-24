806d53d6adbfc9ea0cac7a314c103e1afa66af48
DuRuofu <duruofu@qq.com>
2025-11-24 22:43:33 +0800

    feat(cmake): 添加CMakeLists.txt和package.xml，配置ROS2依赖

---
+++ ros2_ws/src/my_robot_commander_cpp/src/test_moveit.cpp
@@ -0,0 +1,86 @@
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+int main(int argc, char **argv)
+{ 
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<rclcpp::Node>("test_moveit");
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+    auto spinner = std::thread([&executor]() { executor.spin(); });
+
+    auto arm = moveit::planning_interface::MoveGroupInterface(node, "arm");
+    arm.setMaxVelocityScalingFactor(1.0);
+    arm.setMaxAccelerationScalingFactor(1.0);
+
+    // // 设置初始位置和目标位置
+    // arm.setStartStateToCurrentState();
+    // arm.setNamedTarget("pose_1");
+
+    // // 规划运动路径
+    // moveit::planning_interface::MoveGroupInterface::Plan Plan1;
+    // bool success1 = (arm.plan(Plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+
+    // if (success1)
+    // {
+    //     // 运行运动
+    //     arm.execute(Plan1);
+    // }
+    // else
+    // {
+    //     RCLCPP_ERROR(node->get_logger(), "Planning failed");
+    // }
+
+    // ---------------------------------------------------------------------------------
+    // 指定关节角度
+    // std::vector<double> joints = {1.5,0.5,0.0,1.5,0.0,-0.7};
+    // arm.setStartStateToCurrentState();
+    // arm.setJointValueTarget(joints);
+
+    // moveit::planning_interface::MoveGroupInterface::Plan Plan1;
+    // bool success1 = (arm.plan(Plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+    // if (success1)
+    // {
+    //     arm.execute(Plan1);
+    // }
+    // else
+    // {
+    //     RCLCPP_ERROR(node->get_logger(), "Planning failed");
+    // }
+
+
+    // ---------------------------------------------------------------------------------
+    // 指定目标姿态
+    tf2::Quaternion q;
+    q.setRPY(3.14, 0.0, 0.0);
+    q = q.normalize();
+
+    geometry_msgs::msg::PoseStamped target_pose;
+    target_pose.header.frame_id = "base_link";
+    target_pose.pose.position.x = 0.0;
+    target_pose.pose.position.y = -0.7;
+    target_pose.pose.position.z = 0.4;
+    target_pose.pose.orientation.x = q.getX();
+    target_pose.pose.orientation.y = q.getY();
+    target_pose.pose.orientation.z = q.getZ();
+    target_pose.pose.orientation.w = q.getW();
+
+    arm.setStartStateToCurrentState();
+    arm.setPoseTarget(target_pose);
+
+    moveit::planning_interface::MoveGroupInterface::Plan Plan1;
+    bool success1 = (arm.plan(Plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+    if (success1)
+    {
+        arm.execute(Plan1);
+    }
+    else
+    {
+        RCLCPP_ERROR(node->get_logger(), "Planning failed");
+    }
+
+
+    rclcpp::shutdown();
+    spinner.join();
+    return 0;
+}
